16e51917c7337af56553e4289686e5cf706ed78e
hippothomas <hippolyte.thomas1@gmail.com>
2019-10-06 14:41:18 +0200

    Ajout de CompteSimpleDAO

---
+++ src/dal/CompteSimpleDAO.java
@@ -0,0 +1,115 @@
+package dal;
+
+import bo.Agence;
+import bo.CompteSimple;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompteSimpleDAO  implements IDAO<Long, CompteSimple> {
+
+    private static final String INSERT_QUERY = "INSERT INTO compte (solde, decouvert, type, id_agence) VALUES(?,?,?,?)";
+    private static final String UPDATE_QUERY = "UPDATE compte SET solde = ?, decouvert = ? WHERE id = ?";
+    private static final String REMOVE_QUERY = "DELETE FROM compte WHERE id = ?";
+    private static final String FIND_QUERY = "SELECT * FROM compte WHERE id = ?";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM compte";
+
+    @Override
+    public void create(CompteSimple compteSimple) throws SQLException, IOException, ClassNotFoundException {
+
+        Connection connection = PersistenceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection
+                    .prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setFloat(1, compteSimple.getSolde());
+                ps.setFloat(2, compteSimple.getDecouvert());
+                ps.setInt(3, compteSimple.getType());
+                ps.setInt(3, compteSimple.getIdAgence());
+                ps.executeUpdate();
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        compteSimple.setId(rs.getInt(1));
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void update(CompteSimple compteSimple) throws SQLException, IOException, ClassNotFoundException {
+
+        Connection connection = PersistenceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(UPDATE_QUERY)) {
+                ps.setFloat(1, compteSimple.getSolde());
+                ps.setFloat(2, compteSimple.getDecouvert());
+                ps.setInt(3, compteSimple.getId());
+                ps.executeUpdate();
+            }
+        }
+    }
+
+    @Override
+    public void remove(CompteSimple compteSimple) throws SQLException, IOException, ClassNotFoundException {
+        Connection connection = PersistenceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(REMOVE_QUERY)) {
+                ps.setInt(1, compteSimple.getId());
+                ps.executeUpdate();
+            }
+        }
+    }
+
+    @Override
+    public CompteSimple findById(Long id) throws SQLException, IOException, ClassNotFoundException {
+        CompteSimple compteSimple = null;
+        Connection connection = PersistenceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(FIND_QUERY)) {
+                ps.setLong(1, id);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        compteSimple = new CompteSimple();
+                        compteSimple.setId(rs.getInt("id"));
+                        compteSimple.setSolde(rs.getFloat("solde"));
+                        compteSimple.setDecouvert(rs.getFloat("decouvert"));
+                        compteSimple.setType(rs.getInt("type"));
+
+                        AgenceDAO agenceDAO = null;
+                        Agence agence = agenceDAO.findById(rs.getInt("id_agence"));
+                        compteSimple.setAgence(agence);
+                    }
+                }
+            }
+        }
+        return compteSimple;
+    }
+
+    @Override
+    public List<CompteSimple> findAll() throws SQLException, IOException, ClassNotFoundException {
+        List<CompteSimple> list = new ArrayList<>();
+        Connection connection = PersistenceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(FIND_ALL_QUERY)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        CompteSimple compteSimple = new CompteSimple();
+                        compteSimple.setId(rs.getInt("id"));
+                        compteSimple.setSolde(rs.getFloat("solde"));
+                        compteSimple.setDecouvert(rs.getFloat("decouvert"));
+                        compteSimple.setType(rs.getInt("type"));
+
+                        AgenceDAO agenceDAO = null;
+                        Agence agence = agenceDAO.findById(rs.getInt("id_agence"));
+                        compteSimple.setAgence(agence);
+
+                        list.add(compteSimple);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
